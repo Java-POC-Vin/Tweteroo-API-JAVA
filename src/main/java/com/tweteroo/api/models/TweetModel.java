3a358284d09ef0ac21b6301d155c336be85bc4f6
Viniccius Wirz <vinimovel23@gmail.com>
2024-01-27 00:14:32 -0300

    feat: database models

---
+++ src/main/java/com/tweteroo/api/models/TweetModel.java
@@ -0,0 +1,30 @@
+package com.tweteroo.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "tweets")
+public class TweetModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, length = 280)
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private UserModel user;
+}
--- src/main/java/com/tweteroo/api/repositories/TweetRepository.java
+++ src/main/java/com/tweteroo/api/repositories/TweetRepository.java
@@ -1,7 +1,11 @@
 package com.tweteroo.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TweetRepository extends JpaRepository<String, Long> {
+import com.tweteroo.api.models.TweetModel;
+
+@Repository
+public interface TweetRepository extends JpaRepository<TweetModel, Long> {
 
 }
